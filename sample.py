6ae08b4ded0ea361c617f7894a7a13d45ee34d75
Mo-banzu <mbandu.ilik@gmail.com>
2021-03-28 19:55:45 +0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -10,14 +10,11 @@ from liff.ttypes import LiffChatContext, LiffContext, LiffSquareChatContext, Lif
 from mobanzu import Mobanzu
 import requests, uvloop, json, threading, asyncio, livejson
 
-client = LINE("YOUR_EMAIL", "YOUR_PASSWORD")
+client = LINE("EMAIL", "PASSWORD") #USE_YOUR_EMAIL_AND_PASSWORD
 client.log("Auth Token : " + str(client.authToken))
 
-clientProfile = client.getProfile()
-clientSettings = client.getSettings()
-clientPoll = OEPoll(client)
+poll = OEPoll(client)
 mobflex = Mobanzu(client)
-clientMID = client.getProfile().mid
 loop = asyncio.get_event_loop()
 
 def allow_liff():
@@ -26,7 +23,7 @@ def allow_liff():
 	headers = {
 	    'X-Line-Access': client.authToken,
 	    'X-Line-Application': client.server.APP_NAME,
-	    'X-Line-ChannelId': 'CHANNEL_LIFF_ID', #USE_YOUR_CHANNEL_LIFF_ID
+	    'X-Line-ChannelId': 'CHANNEL_ID', #USE_YOUR_CHANNEL_ID
 	    'Content-Type': 'application/json'
 	}
 	requests.post(url, json=data, headers=headers)
@@ -44,13 +41,17 @@ def sendTemplate(to, data):
     data = {"messages": [data]}
     requests.post(url, headers=headers, data=json.dumps(data))
 
+def sendFlex(to, alt, flex):
+    data = {"type": "flex", "altText": alt, "contents": flex}
+    sendTemplate(to, data)
+
 async def clientBot(op):
     try:
         if op.type == 0:
-#            print ("[ 0 ] END OF OPERATION")
+#            print("[ 0 ] END OF OPERATION")
             return
         if op.type == 26:
-            print ("[ 26 ] RECEIVE MESSAGE")
+            print("[ 26 ] RECEIVE MESSAGE")
             msg = op.message
             text = msg.text
             id = msg.id
@@ -59,42 +60,40 @@ async def clientBot(op):
             sender = msg._from
             if msg.toType == 0 or msg.toType == 1 or msg.toType == 2:
                 if msg.toType == 0:
-                    if sender != clientMID: to = sender
+                    if sender != client.getProfile().mid: to = sender
                     else: to = receiver
                 if msg.toType == 1 or msg.toType == 2: to = msg.to
                 if msg.contentType == 0:
                     if None == msg.text: return
                     cmd = msg.text.lower()
                     if cmd == "allowliff":
-                        try:
-                            allow_liff()
-                            client.sendReplyMessage(id, to, "Access Granted For Flex Message.")
+                        try: allow_liff(); client.sendReplyMessage(id, to, "Access Granted For Flex Message.")
                         except: client.sendReplyMessage(id, to, "line://app/LIFF_ID?type=text&text=Done") #USE_YOUR_LIFF_ID
                     try:
                         if cmd == "help":
                             contact = client.getContact(sender)
                             name = contact.displayName
-                            status = contact.statusMessage if contact.statusMessage != '' else 'Unknown'
+                            status = contact.statusMessage if contact.statusMessage != '' else ' '
                             try: picture = "https://obs.line-scdn.net/" + contact.pictureStatus
                             except: picture = "https://i.ibb.co/tczXyp1/hlth-Img-Not-Found.jpg"
-                            mobflex.helpMenu(to, picture, name, status)
-                        if cmd == "profile": mobflex.profileMenu(to)
-                        if cmd == "group": mobflex.groupMenu(to)
-                        if cmd == "media": mobflex.mediaMenu(to)
-                        if cmd == "service": mobflex.serviceMenu(to)
-                        if cmd == "system": mobflex.systemMenu(to)
-                        if cmd == "forum": mobflex.forumMenu(to)
+                            sendFlex(to, "Help Menu", mobflex.helpMenu(picture, name, status))
+                        if cmd == "profile": sendFlex(to, "Profile Menu", mobflex.profileMenu())
+                        if cmd == "group": sendFlex(to, "Group Menu", mobflex.groupMenu())
+                        if cmd == "media": sendFlex(to, "Media Menu", mobflex.mediaMenu())
+                        if cmd == "service": sendFlex(to, "Service Menu", mobflex.serviceMenu())
+                        if cmd == "system": sendFlex(to, "System Menu", mobflex.systemMenu())
+                        if cmd == "forum": sendFlex(to, "Forum Menu", mobflex.forumMenu())
                     except: client.sendReplyMessage(id, to, "Access LIFF Required\nPlease Type 'Allowliff' First.")
     except Exception as error: print(error)
 
 def run():
     while True:
         try:
-            ops = clientPoll.singleTrace(count=50)
+            ops = poll.singleTrace(count=50)
             if ops != None:
                 for op in ops:
                     loop.run_until_complete(clientBot(op))
-                    clientPoll.setRevision(op.revision)
+                    poll.setRevision(op.revision)
         except Exception as error: print(error)
 
 if __name__ == "__main__":
